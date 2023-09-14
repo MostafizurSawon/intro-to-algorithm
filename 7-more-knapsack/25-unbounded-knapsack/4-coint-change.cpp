fda5f68afea5de1bca15a734c45104b208ecc947
Mostafizur06 <mostafizur1102@gmail.com>
2023-09-14 14:40:04 +0600

    wek 7 added

---
+++ 7-more-knapsack/25-unbounded-knapsack/4-coint-change.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int n;
+  cin >> n;
+  int w[n];
+
+  for (int i = 0; i < n; i++)
+  {
+    cin >> w[i];
+  }
+
+  int s;
+  cin >> s;
+  int dp[n + 1][s + 1];
+
+  dp[0][0] = 1;
+  for (int i = 1; i <= s; i++)
+    dp[0][i] = 0;
+
+  for (int i = 1; i <= n; i++)
+  {
+    for (int j = 0; j <= s; j++)
+    {
+      if (w[i - 1] <= j)
+      {
+        dp[i][j] = dp[i][j - w[i - 1]] + dp[i - 1][j];
+      }
+      else
+      {
+        dp[i][j] = dp[i - 1][j];
+      }
+    }
+  }
+
+  cout << dp[n][s] << endl;
+
+  return 0;
+}
+/*
+3
+1 2 3
+5
+=>5
+
+1,1,1,1,1
+1,2,2
+1,1,1,2
+1,1,3
+2,3
+*/
+
+// https://recursion.vercel.app/
